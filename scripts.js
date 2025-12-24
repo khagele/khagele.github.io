3930e946906511dc61a428746234e723971a9178
Kasper Hägele <32416437+khagele@users.noreply.github.com>
2025-12-24 15:22:46 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function setRange(range) {
-  document.querySelectorAll("button").forEach(b => b.classList.remove("active"));
-  document.getElementById(range).classList.add("active");
-
-  document.querySelectorAll("iframe[data-base]").forEach(frame => {
-    frame.src = frame.dataset.base
-      .replace("{FROM}", range === "6h" ? "now-6h" : range === "24h" ? "now-24h" : "now-7d");
-  });
-}
